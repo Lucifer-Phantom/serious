34105e1579886970a70c8f2b71d99b16c0cd828c
Lucifer-Phantom <144218257+Lucifer-Phantom@users.noreply.github.com>
2024-02-01 18:56:22 +0530

    Update serious.js

--- serious.js
+++ serious.js
@@ -17,6 +17,10 @@
    div.innerHTML="hacking all data"
    document.body.appendChild(div)
    resolve()},2000)})
+    await  new Promise((resolve,reject)=>{ setTimeout(()=>{let div=document.createElement('div')
+   div.innerHTML="connecting to facebook...."
+   document.body.appendChild(div)
+   resolve()},2000)})
     
 })()
 
